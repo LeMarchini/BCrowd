0921e573d9d594b7db9e9c03429a0ee1a5166e24
Leandro Marchini <51243216+LeMarchini@users.noreply.github.com>
2022-05-15 15:45:24 -0300

    Add files via upload

---
+++ 1008.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int num = int.Parse(Console.ReadLine());
+        int hr = int.Parse(Console.ReadLine());
+        double val = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double sal = hr * val;
+
+
+        Console.WriteLine("NUMBER = " + num);
+        Console.WriteLine("SALARY = U$ " + sal.ToString("F2", CultureInfo.InvariantCulture));
+
+    }
+
+}
