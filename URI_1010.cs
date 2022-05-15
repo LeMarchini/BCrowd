dbca6cd406483e9965c5ec31cf33deee328f8f28
Leandro Marchini <51243216+LeMarchini@users.noreply.github.com>
2022-05-15 16:02:30 -0300

    Add files via upload

---
+++ URI_1010.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+class URI_1010
+{
+    //BeeCrowd Problem #1010
+
+    static void Main(string[] args)
+    {
+        string[] prod1 = Console.ReadLine().Split(' ');
+        string[] prod2 = Console.ReadLine().Split(' ');
+
+        int c1 = int.Parse(prod1[0]);
+        int n1 = int.Parse(prod1[1]);
+        double p1 = double.Parse(prod1[2], CultureInfo.InvariantCulture);
+
+        int c2 = int.Parse(prod2[0]);
+        int n2 = int.Parse(prod2[1]);
+        double p2 = double.Parse(prod2[2], CultureInfo.InvariantCulture);
+
+        double total = (n1 * p1) + (n2 * p2);
+
+        Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+    }
+
+}
